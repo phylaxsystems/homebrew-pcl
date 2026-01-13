293f20fa0d448b7bf0aae586e7c7f9dd103b4986
makemake <vukasin@gostovic.me>
2026-01-13 12:11:59 +0100

    chore: bump to 0.9.4

--- Formula/phylax.rb
+++ Formula/phylax.rb
@@ -1,29 +1,26 @@
 class Phylax < Formula
-  desc "Phylax Credible Layer (PCL) CLI"
+  desc "Credible Layer CLI"
   homepage "https://github.com/phylaxsystems/credible-sdk"
-  version "0.9.3"
+  version "0.9.4"
 
   # Define URLs and SHA256 checksums for different architectures
   # You will need to update these for each new release.
   # To get the SHA256, download the binary and run: shasum -a 256 <binary_file_name>
 
   if OS.mac?
-    if Hardware::CPU.intel?
-      url "https://github.com/phylaxsystems/credible-sdk/releases/download/#{version}/pcl-#{version}-macos-x86_64.tar.gz"
-      sha256 "41ddd46131b9626762255c75c1aad8fcbc22922d3e097f77e536b10f770bb6c0"
-    elsif Hardware::CPU.arm?
-      url "https://github.com/phylaxsystems/credible-sdk/releases/download/#{version}/pcl-#{version}-macos-arm64.tar.gz"
-      sha256 "e64762127500af01b6302eb37d4567ff515d52e6865735a6385f806b096688c8"
+    if Hardware::CPU.arm?
+      url "https://github.com/phylaxsystems/credible-sdk/releases/download/main/pcl-#{version}-macos-arm64.tar.gz"
+      sha256 "3be04c912694282bcd2ac75ea10ef4d1a7fe9e3c17a4fd165eacbf1a93e20292"
     else
-      odie "Unsupported MacOS architecture."
+      odie "Intel macOS is no longer supported."
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/phylaxsystems/credible-sdk/releases/download/#{version}/pcl-#{version}-linux-x86_64.tar.gz"
-      sha256 "7e22236f7fe90105a3a5c03f98c00865b3efcf56cabe957e64df6cdc488a4926"
+      url "https://github.com/phylaxsystems/credible-sdk/releases/download/main/pcl-#{version}-linux-x86_64.tar.gz"
+      sha256 "690cf4bdefd7b7bd72e8347fea7ff67672eda96dcc21f44c91ded1ecab655e4a"
     elsif Hardware::CPU.arm?
-      url "https://github.com/phylaxsystems/credible-sdk/releases/download/#{version}/pcl-#{version}-linux-arm64.tar.gz"
-      sha256 "d509d07f27a6d6b152b602dc0cd9fed9119b562a1ad7fcd28734b939c5f19ed6"
+      url "https://github.com/phylaxsystems/credible-sdk/releases/download/main/pcl-#{version}-linux-arm64.tar.gz"
+      sha256 "4fb91075326e845fc51d2ac47b564f0b54020e9d9cd3097ea8d8cbb8695472d7"
     else
       odie "Unsupported Linux architecture."
     end
