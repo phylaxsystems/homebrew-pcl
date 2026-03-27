a0536ce5c263744a26e71b21fd89be76bda3ce0c
makemake <vukasin@gostovic.me>
2026-03-27 15:01:08 +0100

    Fix phylax 1.2.0 formula metadata

--- Formula/phylax.rb
+++ Formula/phylax.rb
@@ -3,25 +3,23 @@ class Phylax < Formula
   homepage "https://github.com/phylaxsystems/credible-sdk"
   version "1.2.0"
 
-  if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/phylaxsystems/credible-sdk/releases/download/#{version}/pcl-#{version}-macos-arm64.tar.gz"
-      sha256 ""
-    else
-      odie "Intel macOS is no longer supported."
-    end
-  elsif OS.linux?
-    if Hardware::CPU.intel?
+  on_macos do
+    depends_on arch: :arm64
+
+    url "https://github.com/phylaxsystems/credible-sdk/releases/download/#{version}/pcl-#{version}-macos-arm64.tar.gz"
+    sha256 "dac8590af37cff432cc9e85370399392012d91c479988e34562b890ece70cff6"
+  end
+
+  on_linux do
+    on_intel do
       url "https://github.com/phylaxsystems/credible-sdk/releases/download/#{version}/pcl-#{version}-linux-x86_64.tar.gz"
-      sha256 ""
-    elsif Hardware::CPU.arm?
-      url "https://github.com/phylaxsystems/credible-sdk/releases/download/#{version}/pcl-#{version}-linux-arm64.tar.gz"
-      sha256 ""
-    else
-      odie "Unsupported Linux architecture."
+      sha256 "60565c9211c0bb03fede1b4dc26198b4e93dcf30b1ac637cc02a68693d9f246a"
     end
-  else
-    odie "Unsupported operating system."
+
+    on_arm do
+      url "https://github.com/phylaxsystems/credible-sdk/releases/download/#{version}/pcl-#{version}-linux-arm64.tar.gz"
+      sha256 "651a1d93b20195f7d57c31ddca742701e1db7ffe2c5b94c3dd84def7b3ac6d76"
+    end
   end
 
   def install
